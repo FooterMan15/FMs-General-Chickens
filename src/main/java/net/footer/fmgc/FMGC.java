cbcbe8c2ee5c42ddc75803a01048f4739d87e443
FooterMan13 | FM Studios <71946346+FooterMan15@users.noreply.github.com>
2023-07-15 09:01:34 -0400

    Initial commit

--- src/main/java/net/footer/fmgc/FMGC.java
+++ src/main/java/net/footer/fmgc/FMGC.java
@@ -2,12 +2,9 @@ package net.footer.fmgc;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.ForgeRegistries;
